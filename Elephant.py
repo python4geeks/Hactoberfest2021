b34a730466d76e1e6e834cbde2fc7169c1c5fdc8
Andrew <afmcdnl@gmail.com>
2021-10-26 09:52:22 -0400

    A fun guessing game
    Here is a fun guessing game that I made in Python.  Enjoy!!

---
+++ Elephant.py
@@ -0,0 +1,11 @@
+
+print("Guess the animal!")
+
+guess = ''
+answer = 'elephant'
+while guess != answer:
+        guess = input("Guess? ")
+        
+print("Correct!")
+print("Have a nice day!")
+                 
